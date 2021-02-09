d958bd4bc62313f9877d66129e93fc617dcea403
ZornTaov <zeke_kjell2@yahoo.com>
2021-02-08 21:05:09 -0800

    Initial commit

---
+++ ImgurAlbumExtractor/Contracts.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+namespace ImgurAlbumExtractor
+{
+    [DataContract]
+    class ImgurResult<T>
+    {
+        [DataMember]
+        public T data { get; set; }
+
+        [DataMember]
+        public bool success { get; set; }
+
+        [DataMember]
+        public int status { get; set; }
+}
+    [DataContract]
+    class Image
+    {
+
+        [DataMember]
+        //The ID for the image
+        public string id { get; set; }
+
+        [DataMember]
+        //The title of the image.
+        public string title { get; set; }
+
+        [DataMember]
+        //Description of the image.
+        public string description { get; set; }
+
+        [DataMember]
+        //Image MIME type.
+        public string type { get; set; }
+
+        [DataMember]
+        //is the image animated
+        public bool? animated { get; set; }
+
+        [DataMember]
+        //The width of the image in pixels
+        public int width { get; set; }
+
+        [DataMember]
+        //The height of the image in pixels
+        public int height { get; set; }
+
+        [DataMember]
+        //The size of the image in bytes
+        public int size { get; set; }
+
+        [DataMember]
+        //The direct link to the the image. (Note: if fetching an animated GIF that was over 20MB in original size, a .gif thumbnail will be returned)
+        public string link { get; set; }
+
+        [DataMember(IsRequired = false)]
+        //OPTIONAL, The .gifv link. Only available if the image is animated and type is 'image/gif'.
+        public string gifv { get; set; }
+
+        [DataMember(IsRequired = false)]
+        //OPTIONAL, The direct link to the .mp4. Only available if the image is animated and type is 'image/gif'.
+        public string mp4 { get; set; }
+
+        [DataMember(IsRequired = false)]
+        //OPTIONAL, The Content-Length of the .mp4. Only available if the image is animated and type is 'image/gif'. Note that a zero value (0) is possible if the video has not yet been generated
+        public int mp4_size { get; set; }
+
+        [DataMember(IsRequired = false)]
+        //OPTIONAL, Whether the image has a looping animation. Only available if the image is animated and type is 'image/gif'.
+        public bool? looping { get; set; }
+
+        [DataMember]
+        //Indicates if the image has been marked as nsfw or not. Defaults to null if information is not available.
+        public bool? nsfw { get; set; }
+    }
+}
---
+++ ImgurAlbumExtractor/ImgurMain.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using DreadBot;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace ImgurAlbumExtractor
+{
+    public class ImgurMain : IDreadBotPlugin
+    {
+        public string PluginID { get { return "ImgurAlbumExtractor"; } }
+
+        public void Init()
+        {
+            Events.TextEvent += OnText;
+        }
+
+        internal static void OnText(EventArgs args) 
+        {
+            if (args is MessageEventArgs)
+            {
+                Message msg = (args as MessageEventArgs).msg;
+                if (msg.text.ToLower().StartsWith("/ping"))
+                {
+
+                    Console.WriteLine("pong");
+                }
+                else if (msg.text.ToLower().StartsWith("/showalbum"))
+                {
+                    if (msg.text.ToLower().Contains("imgur.com/gallery"))
+                    {
+                        MatchCollection mc = Regex.Matches(msg.text, "(imgur\\.com\\/gallery.*?)(?>\\s|$)");
+                        if (mc.Count == 0)
+                        {
+                            Console.WriteLine("no gallery link");
+                            return;
+                        }
+                        foreach (Match m in mc)
+                        {
+                            MatchCollection m2 = Regex.Matches(m.Value, "(?>gallery\\/)(.*?)$");
+                            if (m2.Count == 0)
+                            {
+                                Console.WriteLine("no gallery hash");
+                                return;
+                            }
+                            string uriMethod = "https://api.imgur.com/3/album/" + m2[0].Value.Split('/')[1] + "/images";
+                            var client = new HttpClient();
+                            client.DefaultRequestHeaders.Add("Authorization", "Client-ID " + "b265968728f82eb");
+                            var response = Task.Run(() => client.GetAsync(uriMethod)).Result;
+                            client.Dispose();
+                            Console.WriteLine("Got Response from API");
+                            Stream stream = Task.Run(() => response.Content.ReadAsStreamAsync()).Result;
+                            DataContractJsonSerializer dcjs = new DataContractJsonSerializer(typeof(ImgurResult<Image[]>));
+                            try
+                            {
+                                ImgurResult<Image[]> result = (dcjs.ReadObject(stream)) as ImgurResult<Image[]>;
+                                if (result.success)
+                                {
+                                    Console.WriteLine("SUCCESS: found " + result.data.Length + "images");
+                                    int count = result.data.Length;
+                                    int pos = 0;
+                                    while (count > 0)
+                                    {
+                                        List<InputMedia> media = new List<InputMedia>();
+                                        List<InputMedia> gifMedia = new List<InputMedia>();
+                                        for (int i = 0; i < Math.Min(count, 10); i++)
+                                        {
+                                            InputMedia curMedia;
+                                            if (result.data[i + pos].link.EndsWith("mp4")) // should be video
+                                            {
+                                                curMedia = new InputMediaVideo()
+                                                {
+                                                    type = "video",
+                                                    media = result.data[i + pos].link,
+                                                    height = result.data[i + pos].height,
+                                                    width = result.data[i + pos].width
+                                                };
+                                                media.Add(curMedia);
+                                            }
+                                            if (result.data[i + pos].link.EndsWith("gif")) // should be video
+                                            {
+                                                curMedia = new InputMediaAnimation()
+                                                {
+                                                    type = "animation",
+                                                    media = result.data[i + pos].link,
+                                                    height = result.data[i + pos].height,
+                                                    width = result.data[i + pos].width
+                                                };
+                                                gifMedia.Add(curMedia);
+                                            }
+                                            else //should be image?
+                                            {
+                                                curMedia = new InputMediaPhoto()
+                                                {
+                                                    type = "photo",
+                                                    media = result.data[i + pos].link
+                                                };
+                                                media.Add(curMedia);
+                                            }
+                                        }
+
+                                        Methods.sendMediaGroup(msg.chat.id, media.ToArray());
+                                        if (gifMedia.Count() > 0)
+                                        {
+                                            foreach (var gif in gifMedia)
+                                            {
+                                                Methods.sendAnimation(msg.chat.id, gif.media, "");
+                                            }
+                                        }
+                                        pos += 10;
+                                        count -= 10;
+                                    }
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                                Console.WriteLine("Not an album");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("no gallery link");
+                    }
+                }
+            }
+        }
+        public void PostInit()
+        {
+
+        }
+    }
+}
