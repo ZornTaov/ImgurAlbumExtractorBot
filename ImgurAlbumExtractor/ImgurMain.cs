9465df91125935366ef410b7fd39bf8b1142149d
ZornTaov <zeke_kjell2@yahoo.com>
2023-04-16 11:24:03 -0700

    Updated stuff, forgot what
    I need to be more proactive with my commits, whoops.

--- ImgurAlbumExtractor/ImgurMain.cs
+++ ImgurAlbumExtractor/ImgurMain.cs
@@ -19,6 +19,7 @@ namespace ImgurAlbumExtractor
         public void Init()
         {
             Events.TextEvent += OnText;
+            Logger.CurrentLogLevel = LogLevel.Debug;
         }
 
         internal static void OnText(EventArgs args) 
@@ -29,123 +30,141 @@ namespace ImgurAlbumExtractor
                 if (msg.text.ToLower().StartsWith("/ping"))
                 {
                     Methods.sendMessage(msg.chat.id, "pong");
+                    Logger.LogInfo("pong");
                 }
-                else if (msg.text.ToLower().StartsWith("/showalbum") || msg.text.ToLower().Contains("imgur.com/gallery"))
+                else if (msg.text.ToLower().StartsWith("/showalbum") || msg.text.ToLower().Contains("imgur.com/gallery") 
+                    || msg.text.ToLower().Contains("imgur.com/a/"))
                 {
-                    MatchCollection mc = Regex.Matches(msg.text, "(imgur\\.com\\/gallery.*?)(?>\\s|$)");
-                    if (mc.Count == 0)
-                    {
-                        Methods.sendMessage(msg.chat.id, "no gallery link");
-                        return;
-                    }
-                    foreach (Match m in mc)
-                    {
-                        MatchCollection m2 = Regex.Matches(m.Value, "(?>gallery\\/)(.*?)$");
-                        if (m2.Count == 0)
-                        {
-                            Methods.sendMessage(msg.chat.id, "no gallery hash");
-                            return;
-                        }
-                        string uriMethod = "https://api.imgur.com/3/album/" + m2[0].Value.Split('/')[1] + "/images";
-                        var client = new HttpClient();
-                        client.DefaultRequestHeaders.Add("Authorization", "Client-ID " + "b265968728f82eb");
-                        var response = Task.Run(() => client.GetAsync(uriMethod)).Result;
-                        client.Dispose();
-                        Console.WriteLine("Got Response from API");
-                        Stream stream = Task.Run(() => response.Content.ReadAsStreamAsync()).Result;
-                        DataContractJsonSerializer dcjs = new DataContractJsonSerializer(typeof(ImgurResult<Image[]>));
-                        try
-                        {
-                            ImgurResult<Image[]> result = (dcjs.ReadObject(stream)) as ImgurResult<Image[]>;
-                            if (result.success)
-                            {
-                                Methods.sendMessage(msg.chat.id, "SUCCESS: found " + result.data.Length + "images! Give me a moment to send them all!");
-                                Methods.sendChatAction(msg.chat.id, "typing");
-                                Console.WriteLine("SUCCESS: found " + result.data.Length + "images");
-                                int count = result.data.Length;
-                                int pos = 0;
-                                while (count > 0)
-                                {
-                                    int subcount = 0;
-                                    List<InputMedia> media = new List<InputMedia>();
-                                    List<InputMedia> gifMedia = new List<InputMedia>();
-                                    List<InputMedia> videoMedia = new List<InputMedia>();
-                                    for (int i = 0; i < Math.Min(count, 10); i++)
-                                    {
-                                        InputMedia curMedia;
-                                        if (result.data[i + pos].link.EndsWith("mp4")) // should be video
-                                        {
-                                            curMedia = new InputMediaVideo()
-                                            {
-                                                type = "video",
-                                                media = result.data[i + pos].link,
-                                                height = result.data[i + pos].height,
-                                                width = result.data[i + pos].width
-                                            };
-                                            videoMedia.Add(curMedia);
-                                            subcount++;
-                                            break;
-                                        }
-                                        if (result.data[i + pos].link.EndsWith("gif")) // should be video
-                                        {
-                                            curMedia = new InputMediaAnimation()
-                                            {
-                                                type = "animation",
-                                                media = result.data[i + pos].link,
-                                                height = result.data[i + pos].height,
-                                                width = result.data[i + pos].width
-                                            };
-                                            gifMedia.Add(curMedia);
-                                            subcount++;
-                                            break;
-                                        }
-                                        else //should be image?
-                                        {
-                                            curMedia = new InputMediaPhoto()
-                                            {
-                                                type = "photo",
-                                                media = result.data[i + pos].link
-                                            };
-                                            media.Add(curMedia);
-                                            subcount++;
-                                        }
-                                    }
-
-                                    if (media.Count() > 0)
-                                    {
-                                        Methods.sendChatAction(msg.chat.id, "upload_photo");
-                                        Methods.sendMediaGroup(msg.chat.id, media.ToArray());
-                                    }
-                                    if (gifMedia.Count() > 0)
-                                    {
-                                        foreach (var gif in gifMedia)
-                                        {
-                                            Methods.sendChatAction(msg.chat.id, "upload_video");
-                                            Methods.sendAnimation(msg.chat.id, gif.media, "");
-                                        }
-                                    }
-                                    if (videoMedia.Count() > 0)
-                                    {
-                                        foreach (var vid in videoMedia)
-                                        {
-                                            Methods.sendChatAction(msg.chat.id, "upload_video");
-                                            Methods.sendAnimation(msg.chat.id, vid.media, "");
-                                        }
-                                    }
-                                    pos += subcount;
-                                    count -= subcount;
-                                }
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.Message);
-                            Methods.sendMessage(msg.chat.id, "Not an album");
-                        }
-                    }
+                    Task.Run(() => ParseAlbum(msg));
                 }
             }
         }
+
+        private static void ParseAlbum(Message msg)
+        {
+            MatchCollection mc = Regex.Matches(msg.text, "(imgur\\.com\\/(?>gallery|a).*?)(?>\\s|$)");
+            if (mc.Count == 0)
+            {
+                Methods.sendMessage(msg.chat.id, "no gallery link");
+                Logger.LogInfo("no gallery link");
+                return;
+            }
+            foreach (Match m in mc)
+            {
+                MatchCollection m2 = Regex.Matches(m.Value, "(?>gallery|a)\\/(.*?)$");
+                if (m2.Count == 0)
+                {
+                    Methods.sendMessage(msg.chat.id, "no gallery hash");
+                    Logger.LogInfo("no gallery hash");
+                    continue;
+                }
+                string uriMethod = "https://api.imgur.com/3/album/" + m2[0].Value.Split('/')[1] + "/images";
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("Authorization", "Client-ID " + "b265968728f82eb");
+                var response = Task.Run(() => client.GetAsync(uriMethod)).Result;
+                client.Dispose();
+                Logger.LogInfo("Got Response from API");
+                Stream stream = Task.Run(() => response.Content.ReadAsStreamAsync()).Result;
+                ExtractAlbum(msg, stream);
+            }
+        }
+
+        private static void ExtractAlbum(Message msg, Stream stream)
+        {
+            DataContractJsonSerializer dcjs = new DataContractJsonSerializer(typeof(ImgurResult<Image[]>));
+            try
+            {
+                ImgurResult<Image[]> result = (dcjs.ReadObject(stream)) as ImgurResult<Image[]>;
+                if (result.success)
+                {
+                    Methods.sendMessage(msg.chat.id, "SUCCESS: found " + result.data.Length + "images! Give me a moment to send them all!");
+                    Methods.sendChatAction(msg.chat.id, "typing");
+                    Logger.LogInfo("SUCCESS: found " + result.data.Length + "images");
+                    int count = result.data.Length;
+                    int pos = 0;
+                    while (count > 0)
+                    {
+                        int subcount = 0;
+                        List<InputMedia> media = new List<InputMedia>();
+                        List<InputMedia> gifMedia = new List<InputMedia>();
+                        List<InputMedia> videoMedia = new List<InputMedia>();
+                        for (int i = 0; i < Math.Min(count, 10); i++)
+                        {
+                            InputMedia curMedia;
+                            if (result.data[i + pos].link.EndsWith("mp4")) // should be video
+                            {
+                                curMedia = new InputMediaVideo()
+                                {
+                                    type = "video",
+                                    media = result.data[i + pos].link,
+                                    height = result.data[i + pos].height,
+                                    width = result.data[i + pos].width
+                                };
+                                videoMedia.Add(curMedia);
+                                subcount++;
+                                break;
+                            }
+                            if (result.data[i + pos].link.EndsWith("gif")) // should be video
+                            {
+                                curMedia = new InputMediaAnimation()
+                                {
+                                    type = "animation",
+                                    media = result.data[i + pos].link,
+                                    height = result.data[i + pos].height,
+                                    width = result.data[i + pos].width
+                                };
+                                gifMedia.Add(curMedia);
+                                subcount++;
+                                break;
+                            }
+                            else //should be image?
+                            {
+                                curMedia = new InputMediaPhoto()
+                                {
+                                    type = "photo",
+                                    media = result.data[i + pos].link
+                                };
+                                media.Add(curMedia);
+                                subcount++;
+                            }
+                        }
+
+                        if (media.Count() > 0)
+                        {
+                            Methods.sendChatAction(msg.chat.id, "upload_photo");
+                            Methods.sendMediaGroup(msg.chat.id, media.ToArray());
+                            Task.Delay(1000);
+                        }
+                        if (gifMedia.Count() > 0)
+                        {
+                            foreach (var gif in gifMedia)
+                            {
+                                Methods.sendChatAction(msg.chat.id, "upload_video");
+                                Methods.sendAnimation(msg.chat.id, gif.media, "");
+                                Task.Delay(1000);
+                            }
+                        }
+                        if (videoMedia.Count() > 0)
+                        {
+                            foreach (var vid in videoMedia)
+                            {
+                                Methods.sendChatAction(msg.chat.id, "upload_video");
+                                Methods.sendAnimation(msg.chat.id, vid.media, "");
+                                Task.Delay(1000);
+                            }
+                        }
+                        pos += subcount;
+                        count -= subcount;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.LogInfo(e.Message);
+                Methods.sendMessage(msg.chat.id, "Not an album");
+            }
+        }
+
         public void PostInit()
         {
 
